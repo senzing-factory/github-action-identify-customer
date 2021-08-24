5008aaf0d57819671f50af1fe67ac2c7ad7354c8
SamMacy <49598357+SamMacy@users.noreply.github.com>
2021-08-24 16:46:08 -0400

    Update send-to-slack.py

--- send-to-slack.py
+++ send-to-slack.py
@@ -7,6 +7,8 @@ slack_bearer_token = os.getenv('TOKEN')
 slack_channel = "Playground"
 repo = os.getenv('REPO_URL')
 issue_number = os.getenv('NUMBER')
+maker = os.getenv('CREATOR')
+title = os.getenv('TITLE')
 repo_url = "https://github.com/" +repo
 issue = "https://github.com/" +repo + "/issues/" + issue_number
 
@@ -20,7 +22,7 @@ print(os.getenv('TITLE'))
 
 slack_client = slack.WebClient(token=slack_bearer_token)
 
-slack_message = "New customer submitted issue in <" + repo_url + "|" + repo+ ">\n issue number <" + issue + "|" + issue_number + ">"
+slack_message = "New customer submitted issue \"" + title + "\"/n Created by " +  maker + " in <" + repo_url + "|" + repo+ "> issue number <" + issue + "|" + issue_number + ">(@here)"
 
 response = slack_client.chat_postMessage(
                 channel=slack_channel,
