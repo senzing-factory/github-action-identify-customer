c3a5953dd50c9426f8e40eb6915b7a3abe356e93
SamMacy <49598357+SamMacy@users.noreply.github.com>
2021-08-26 12:03:54 -0400

    #3 update script

--- send-to-slack.py
+++ send-to-slack.py
@@ -4,19 +4,13 @@ import slack
 import os 
 
 slack_bearer_token = os.getenv('TOKEN')
-slack_channel = "Playground"
 repo = os.getenv('REPO_URL')
 issue_number = os.getenv('NUMBER')
 maker = os.getenv('CREATOR')
 title = os.getenv('TITLE')
+slack_channel = "Playground"
 repo_url = "https://github.com/" +repo
 issue = "https://github.com/" +repo + "/issues/" + issue_number
-
-print(issue)
-print(os.getenv('BOOL'))
-print(os.getenv('CREATOR'))
-print(os.getenv('TITLE'))
-
 
 # Create a client that communicates with Slack.
 
