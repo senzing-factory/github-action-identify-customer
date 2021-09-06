1930fe4debc0794d19830aa8e523ef15b549a026
SamMacy <49598357+SamMacy@users.noreply.github.com>
2021-09-06 12:58:40 -0400

    #3 check CODEOWNERS

--- send-to-slack.py
+++ send-to-slack.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python3
+ #! /usr/bin/env python3
 
 import urllib
 import slack
@@ -15,19 +16,53 @@ slack_channel = "Playground"
 repo_url = "https://github.com/" +repo
 issue = "https://github.com/" +repo + "/issues/" + issue_number
 user_url = "https://github.com/" + maker
-codeowners = "https://raw.githubusercontent.com/" + repo_url + "/master/.github/CODEOWNERS"
+codeowners_url = "https://raw.githubusercontent.com/" + repo_url + "/master/.github/CODEOWNERS"
+printmap = "{"
+codeowners = "{ "
+count = 1
 
 
-file = urllib.request.urlopen(codeowners)
+hash_json = json.loads(slack_string)
+file = urllib.request.urlopen(codeowners_url)
 
 for line in file:
-	decoded_line = line.decode("utf-8")
-	echo(decoded_line)
-    
-    
-hash_json = json.loads(slack_string)
+    decoded_line = line.decode("utf-8")
+    if("@" in decoded_line):
+      name = decoded_line.split("@",1)[1]
+      codeowners = codeowners + "\"owners" + str(count) + "\":" + "\""+ name.strip() + "\","
+      count+=1
 
-slack_users = has_json['Senzing/senzing-engineering']
+codeowners = codeowners[:-1]
+codeowners = codeowners + "}"
+
+codeowners_json = json.loads(codeowners)
+
+count = 1
+for key in codeowners_json:
+  value = codeowners_json[key]
+  if( "/" in value):
+    group = value
+    
+for key in codeowners_json:
+  value = codeowners_json[key]
+  if (value not in hash_map[group] and "/" not in value):
+    printmap = printmap + "\"owners" + str(count) + "\":" + "\"" + value + "\","
+    count+=1
+
+for key in hash_map[group]:
+  printmap = printmap + "\"owners" + str(count) + "\":" + "\""+ key +"\","
+  count+=1
+  
+  
+printmap = printmap[:-1]
+printmap = printmap + "}"
+user_json = json.loads(printmap)
+
+for key in user_json:
+  value = user_json[key]
+  print(hash_map[value])
+
+
 # Create a client that communicates with Slack.
 
 slack_client = slack.WebClient(token=slack_bearer_token)
