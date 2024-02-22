dbbe6b7d44744bfbe50dc011cba39e0f2fa055a6
Sam <109683132+kernelsam@users.noreply.github.com>
2024-02-21 17:06:46 -0800

    senzing-factory/build-resources#69 add linting

--- send-to-slack.py
+++ send-to-slack.py
@@ -2,53 +2,71 @@
 
 import urllib
 import slack
-import os 
+import os
 import json
 
-slack_bearer_token = os.getenv('TOKEN')
-repo = os.getenv('REPO_URL')
-issue_number = os.getenv('NUMBER')
-maker = os.getenv('CREATOR')
-title = os.getenv('TITLE')
-slack_string = os.getenv('SLACK_HASHES')
+slack_bearer_token = os.getenv("TOKEN")
+repo = os.getenv("REPO_URL")
+issue_number = os.getenv("NUMBER")
+maker = os.getenv("CREATOR")
+title = os.getenv("TITLE")
+slack_string = os.getenv("SLACK_HASHES")
 slack_channel = "community-notifications"
-repo_url = "https://github.com/" +repo
-issue = "https://github.com/" +repo + "/issues/" + issue_number
+repo_url = "https://github.com/" + repo
+issue = "https://github.com/" + repo + "/issues/" + issue_number
 user_url = "https://github.com/" + maker
 result = []
 
-codeowners_url = "https://raw.githubusercontent.com/" + repo + "/main/.github/CODEOWNERS"
+codeowners_url = (
+    "https://raw.githubusercontent.com/" + repo + "/main/.github/CODEOWNERS"
+)
 try:
-  codeowners = urllib.request.urlopen(codeowners_url)
+    codeowners = urllib.request.urlopen(codeowners_url)
 except:
-  codeowners_url = "https://raw.githubusercontent.com/" + repo + "/main/.github/CODEOWNERS"
-  codeowners = urllib.request.urlopen(codeowners_url)
+    codeowners_url = (
+        "https://raw.githubusercontent.com/" + repo + "/main/.github/CODEOWNERS"
+    )
+    codeowners = urllib.request.urlopen(codeowners_url)
 
 SENZING_GITHUB_SLACK_MAP = json.loads(slack_string)
 
 codeowners = urllib.request.urlopen(codeowners_url)
 
 for line in codeowners:
-        decoded_line = line.decode('utf-8')
-        uncommented_line = decoded_line.split("#")[0]
-        split_line = uncommented_line.split()
-        for split in split_line:
-            key = split.replace("@","")
-            if key in SENZING_GITHUB_SLACK_MAP.keys():
-                values = SENZING_GITHUB_SLACK_MAP.get(key)
-                if type(values) != list:
-                    values = [values]
-                for value in values:
-                    if not value in result:
-                        result.append(value)
-    
-slack_message = "Customer created GitHub issue:\n*Repository:* <" + repo_url + "|" + repo + ">\n      *Customer*: <" + user_url + "|" + maker + ">\n      *Issue:* <" + issue + "|" + title + ">\n      *Attention*: "
+    decoded_line = line.decode("utf-8")
+    uncommented_line = decoded_line.split("#")[0]
+    split_line = uncommented_line.split()
+    for split in split_line:
+        key = split.replace("@", "")
+        if key in SENZING_GITHUB_SLACK_MAP.keys():
+            values = SENZING_GITHUB_SLACK_MAP.get(key)
+            if type(values) != list:
+                values = [values]
+            for value in values:
+                if not value in result:
+                    result.append(value)
+
+slack_message = (
+    "Customer created GitHub issue:\n*Repository:* <"
+    + repo_url
+    + "|"
+    + repo
+    + ">\n      *Customer*: <"
+    + user_url
+    + "|"
+    + maker
+    + ">\n      *Issue:* <"
+    + issue
+    + "|"
+    + title
+    + ">\n      *Attention*: "
+)
 
 for value in result:
     slack_message = slack_message + "<@" + value + ">"
-    
+
 # Create a client that communicates with Slack.
 
 slack_client = slack.WebClient(token=slack_bearer_token)
 
-response = slack_client.chat_postMessage( channel=slack_channel, text=slack_message )
+response = slack_client.chat_postMessage(channel=slack_channel, text=slack_message)
