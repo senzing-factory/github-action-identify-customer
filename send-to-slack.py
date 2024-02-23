10a7e1452624cb5026f93a69d3880928767a91ca
Sam <109683132+kernelsam@users.noreply.github.com>
2024-02-23 05:23:18 -0800

    senzing-factory/build-resources#69 add linting (#22)
    * senzing-factory/build-resources#69 add linting
    
    * senzing-factory/build-resources#69 add linting
    
    * update linting errors
    
    * update linting errors
    
    * test linting changes
    
    * add missing workflow
    
    * fix membership test

--- send-to-slack.py
+++ send-to-slack.py
@@ -1,54 +1,66 @@
 #! /usr/bin/env python3
 
-import urllib
-import slack
-import os 
 import json
+import os
+import urllib.request
 
-slack_bearer_token = os.getenv('TOKEN')
-repo = os.getenv('REPO_URL')
-issue_number = os.getenv('NUMBER')
-maker = os.getenv('CREATOR')
-title = os.getenv('TITLE')
-slack_string = os.getenv('SLACK_HASHES')
+import slack
+
+slack_bearer_token = os.getenv("TOKEN")
+repo = os.getenv("REPO_URL")
+issue_number = os.getenv("NUMBER")
+maker = os.getenv("CREATOR")
+title = os.getenv("TITLE")
+slack_string = os.getenv("SLACK_HASHES")
 slack_channel = "community-notifications"
-repo_url = "https://github.com/" +repo
-issue = "https://github.com/" +repo + "/issues/" + issue_number
-user_url = "https://github.com/" + maker
+repo_url = "https://github.com/{}".format(repo)
+issue = "https://github.com/{}/issues/{}".format(repo, issue_number)
+user_url = "https://github.com/{}".format(maker)
 result = []
 
-codeowners_url = "https://raw.githubusercontent.com/" + repo + "/main/.github/CODEOWNERS"
+codeowners_url = "https://raw.githubusercontent.com/{}/main/.github/CODEOWNERS".format(
+    repo
+)
 try:
-  codeowners = urllib.request.urlopen(codeowners_url)
-except:
-  codeowners_url = "https://raw.githubusercontent.com/" + repo + "/main/.github/CODEOWNERS"
-  codeowners = urllib.request.urlopen(codeowners_url)
+    codeowners = urllib.request.urlopen(codeowners_url)
+except BaseException:
+    codeowners_url = (
+        "https://raw.githubusercontent.com/{}/main/.github/CODEOWNERS".format(repo)
+    )
+    codeowners = urllib.request.urlopen(codeowners_url)
 
-SENZING_GITHUB_SLACK_MAP = json.loads(slack_string)
+SENZING_GITHUB_SLACK_MAP = json.loads(str(slack_string))
 
 codeowners = urllib.request.urlopen(codeowners_url)
 
 for line in codeowners:
-        decoded_line = line.decode('utf-8')
-        uncommented_line = decoded_line.split("#")[0]
-        split_line = uncommented_line.split()
-        for split in split_line:
-            key = split.replace("@","")
-            if key in SENZING_GITHUB_SLACK_MAP.keys():
-                values = SENZING_GITHUB_SLACK_MAP.get(key)
-                if type(values) != list:
-                    values = [values]
-                for value in values:
-                    if not value in result:
-                        result.append(value)
-    
-slack_message = "Customer created GitHub issue:\n*Repository:* <" + repo_url + "|" + repo + ">\n      *Customer*: <" + user_url + "|" + maker + ">\n      *Issue:* <" + issue + "|" + title + ">\n      *Attention*: "
+    decoded_line = line.decode("utf-8")
+    uncommented_line = decoded_line.split("#")[0]
+    split_line = uncommented_line.split()
+    for split in split_line:
+        key = split.replace("@", "")
+        if key in SENZING_GITHUB_SLACK_MAP.keys():
+            values = SENZING_GITHUB_SLACK_MAP.get(key)
+            if type(values) != list:
+                values = [values]
+            for value in values:
+                if value not in result:
+                    result.append(value)
+
+slack_message = (
+    "Customer created GitHub issue:\n*Repository:* <"
+    "{0}|{1}>\n      *Customer*: <"
+    "{2}|{3}>\n      *Issue:* <"
+    "{4}|{5}>\n      *Attention*: ".format(
+        repo_url, repo, user_url, maker, issue, title
+    )
+)
 
 for value in result:
     slack_message = slack_message + "<@" + value + ">"
-    
+
 # Create a client that communicates with Slack.
 
 slack_client = slack.WebClient(token=slack_bearer_token)
 
-response = slack_client.chat_postMessage( channel=slack_channel, text=slack_message )
+response = slack_client.chat_postMessage(channel=slack_channel, text=slack_message)
